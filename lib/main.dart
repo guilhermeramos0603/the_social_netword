468be9cb1b5e6c9c2278a2483b4cc147a4708eec
guilhermeramos0603 <guilherme.silvano@hotmail.com>
2024-11-23 16:35:36 -0300

    feat: login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:the_social_network/presentation/app.dart';
 
-void main() {
-runApp(const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+      apiKey: "AIzaSyCv8V01nd2yK7jmwoL_nhaVUx1LPIcou1M",
+      authDomain: "the-social-network-436c7.firebaseapp.com",
+      projectId: "the-social-network-436c7",
+      storageBucket: "the-social-network-436c7.firebasestorage.app",
+      messagingSenderId: "698999836444",
+      appId: "1:698999836444:web:8cbf9ef63cd780624134d9",
+      measurementId: "G-W0Y9JS0Q21",
+    ));
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(const App());
 }
---
+++ lib/presentation/pages/mobile/mobile_login_page.dart
@@ -0,0 +1,104 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:the_social_network/app/util/colors.dart';
+import 'package:the_social_network/presentation/components/text_field_input.dart';
+
+class MobileLoginPage extends StatefulWidget {
+  const MobileLoginPage({super.key});
+
+  @override
+  State<MobileLoginPage> createState() => _MobileLoginPageState();
+}
+
+class _MobileLoginPageState extends State<MobileLoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            children: [
+              Flexible(
+                flex: 2,
+                child: Container(),
+              ),
+              SvgPicture.asset(
+                'assets/ic_instagram.svg',
+                color: primaryColor,
+                height: 64,
+              ),
+              const SizedBox(
+                height: 64,
+              ),
+              TextFieldInput(
+                  textEditingController: _emailController,
+                  hintText: "Enter your E-mail",
+                  textInputType: TextInputType.emailAddress,
+                  isPass: false),
+              const SizedBox(
+                height: 24,
+              ),
+              TextFieldInput(
+                  textEditingController: _passController,
+                  hintText: "Enter your password",
+                  textInputType: TextInputType.text,
+                  isPass: true),
+              const SizedBox(
+                height: 24,
+              ),
+              Container(
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                decoration: const ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(4),
+                    ),
+                  ),
+                  color: blueColor,
+                ),
+                child: const Text("Log in"),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Flexible(
+                flex: 2,
+                child: Container(),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: const Text("Don't have an account? "),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: const Text(
+                        "Sign Up.",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
